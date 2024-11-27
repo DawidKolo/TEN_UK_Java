f42aebf46dd9d45e7556e1f6e3a276e0c863ed49
DawidKolo <dawid_kolodziej@o2.pl>
2024-11-27 10:49:28 +0100

    ifExists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,34 @@
 package com.company;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-
-
 public class Main {
-    static void myFunc() {
-        File tmpFile = new File("d:\\000\\test.txt");
+
+
+    static void checkFileExists() {
+
+        Path path = Paths.get("d:\\000");
+        String xlsFile = "\\month.xlsx";
+        File tmpFile = new File(path + xlsFile);
         boolean exists = tmpFile.exists();
         if (exists) {
             System.out.println("File is there");
 
         } else {
-            System.out.println("the file is not there");
+            System.out.println(path + xlsFile);
 
         }
 
     }
 
     public static void main(String[] args) throws IOException {
-        myFunc();
+        checkFileExists();
         System.out.println("it works");
     }
 }
